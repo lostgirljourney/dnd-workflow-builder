e5313a5c0cbbe9e3421107dd57eb2e883f503703
Falguni Sarkar <falgunisarkar526@gmail.com>
2024-06-24 20:10:19 +0530

    init

--- src/components/pages/workflowCanvas/WFBuilderNavbar.tsx
+++ src/components/pages/workflowCanvas/WFBuilderNavbar.tsx
@@ -8,14 +8,14 @@ import WorkflowBuilderContext from '../../../pages/contexts/WorkflowBuilderConte
 import { Link } from 'react-router-dom';
 import Papa from 'papaparse';
 
-const modules = import.meta.glob('/src/csv/*.csv', {
+const modules = import.meta.glob('/public/csv/*.csv', {
 	eager: true,
 	import: 'default',
 	query: '?raw'
 });
 
 const fileNames = Object.keys(modules).map((path) =>
-	path.replace('/src/csv/', '')
+	path.replace('/public/csv/', '')
 );
 
 const WFBuilderNavbar: React.FC<NavbarProps> = ({ menus, active }) => {
@@ -29,7 +29,7 @@ const WFBuilderNavbar: React.FC<NavbarProps> = ({ menus, active }) => {
 	const [setShowToast] = saveBtn.toast;
 
 	const onClick = (file: string) => {
-		Papa.parse(`/src/csv/${file}?url&raw`, {
+		Papa.parse(`/csv/${file}?url&raw`, {
 			download: true,
 			header: true,
 			complete: (results) => {
